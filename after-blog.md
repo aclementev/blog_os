3a11f5b01b70ca767da0731f77fc5943db4da517
Alvaro Clemente <clemente@bigml.com>
2022-08-22 20:08:43 +0200

    feat: basic vga_buffer writing

--- after-blog.md
+++ after-blog.md
@@ -3,3 +3,4 @@
 * Implementing a bootloader (remove **bootimage** crate)
     * Make it simple
     * Add multiboot support later
+* Improve the VGA text mode driver
--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,9 @@
 #![no_std]
 #![no_main]
 
+mod vga_buffer;
+
 use core::panic::PanicInfo;
-
-static HELLO: &[u8] = b"Hello World!";
-
-const VGA_TEXT_BUFFER_START: usize = 0xb8000;
 
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
@@ -14,14 +12,7 @@ fn panic(_info: &PanicInfo) -> ! {
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    let vga_buffer = VGA_TEXT_BUFFER_START as *mut u8;
-
-    for (i, &byte) in HELLO.iter().enumerate() {
-        unsafe {
-            *vga_buffer.offset(i as isize * 2) = byte;
-            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
-        }
-    }
+    vga_buffer::print_something();
 
     #[allow(clippy::empty_loop)]
     loop {}
